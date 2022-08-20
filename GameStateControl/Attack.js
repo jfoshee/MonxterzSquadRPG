bb63f9d29829eda78ab8ccc7270127400c73f941
Jacob Foshee <jacobf@gmail.com>
2022-08-20 15:23:00 -0500

    test(gsc): Initialize character state in attack test so that the test is easier to understand and verify
    Monxterz.StatePlatform commit:
    ebc8b3bca5bdb21ab10f1a4d4d3fb40aa5db917c

--- GameStateControl/Attack.js
+++ GameStateControl/Attack.js
@@ -1,6 +1,6 @@
 /** Attack an enemy in the same battle */
 export function mutate(context) {
   const attacker = context.entities[0].customStatePublic[context.authorId];
-  const enemy = context.entities[1].customStatePublic[context.authorId];
-  enemy.hp -= attacker.strength;
+  const defender = context.entities[1].customStatePublic[context.authorId];
+  defender.hp -= attacker.strength;
 }
--- GameStateControl/Monxterz.SquadRpg.GameState.Tests/AttackTest.cs
+++ GameStateControl/Monxterz.SquadRpg.GameState.Tests/AttackTest.cs
@@ -5,18 +5,21 @@ public class AttackTest
     [Theory, GameTest("monxterz-squad-rpg")]
     public async Task BasicAttack(IGameTestHarness game)
     {
-        // TODO: Initialize attacker strength
         GameEntityState attacker = await game.Create.Character();
-        GameEntityState enemy = await game.Create.Character();
+        GameEntityState defender = await game.Create.Character();
+        // Initialize attacker strength & defender hp
+        game.State(attacker).strength = 13;
+        game.State(defender).hp = 17;
 
-        await game.Call.Attack(attacker, enemy);
+        await game.Call.Attack(attacker, defender);
 
         // Enemy's hp should be reduced by the attacker's strength
-        Assert.Equal(99, game.State(enemy).hp);
+        Assert.Equal(4, game.State(defender).hp);
     }
 
     // TODO: Ensure in same battle
     // TODO: Handle target(s) not passed
     // TODO: Handle not your turn
     // TODO: Handle not your character
+    // TODO: Handle if character killed
 }
