73c54bc0cf297257421911858f07ac40b08d4911
Jacob Foshee <jacobf@gmail.com>
2022-08-20 09:25:14 -0500

    test(gsc): Add new character test

---
+++ GameStateControl/Monxterz.SquadRpg.GameState.Tests/CharacterTest.cs
@@ -0,0 +1,14 @@
+namespace Monxterz.SquadRpg.GameState.Tests;
+
+public class CharacterTest
+{
+    [Theory, GameTest("monxterz-squad-rpg")]
+    public async Task DefaultCharacter(IGameTestHarness game)
+    {
+        GameEntityState character = await game.Create.Character();
+        var characterState = game.State(character);
+        Assert.Equal(100, characterState.hp);
+        Assert.Equal(0, characterState.xp);
+        Assert.Equal(1, characterState.strength);
+    }
+}
