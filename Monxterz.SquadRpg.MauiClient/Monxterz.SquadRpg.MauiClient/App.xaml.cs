7655b1ab27fcef00b7c27160df02481560ca7aae
Jacob Foshee <jacobf@gmail.com>
2022-08-21 19:11:16 -0500

    feat(app): New .NET MAUI App

---
+++ Monxterz.SquadRpg.MauiClient/Monxterz.SquadRpg.MauiClient/App.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace Monxterz.SquadRpg.MauiClient;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+
+		MainPage = new AppShell();
+	}
+}
---
+++ Monxterz.SquadRpg.MauiClient/Monxterz.SquadRpg.MauiClient/AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace Monxterz.SquadRpg.MauiClient;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
