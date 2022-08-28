8222d5c292088517ccd98902fbdf386fb9b9d3df
Jacob Foshee <jacobf@gmail.com>
2022-08-28 13:58:42 -0500

    feat(blazor): Show only first line of error message to avoid showing the call stack

---
+++ Monxterz.SquadRpg.BlazorClient/Monxterz.SquadRpg.GameClient/StringExtensions.cs
@@ -0,0 +1,10 @@
+﻿namespace Monxterz.SquadRpg.GameClient;
+
+public static class StringExtensions
+{
+    public static string FirstLine(this string str)
+    {
+        using var reader = new StringReader(str);
+        return reader.ReadLine();
+    }
+}
